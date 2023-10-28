c7bb07998857f24225285524f062f3765d402712
Jack Najarro <jacknajarro1@gmail.com>
2023-10-28 12:50:51 -0500

    paddingLeft

--- app/(main)/_components/item.jsx
+++ app/(main)/_components/item.jsx
@@ -85,7 +85,7 @@ export default function Item({ label, onClick, icon: Icon, active, id, documentI
 
 Item.Skeleton = function ItemSkeleton({ level }) {
   return (
-    <div style={{ padding: level ? `${(level * 12) + 25}px` : '12px' }} className='flex gap-x-2 py-[3px]'>
+    <div style={{ paddingLeft: level ? `${(level * 12) + 5}px` : '12px' }} className='flex gap-x-2 py-[3px]'>
       <Skeleton className='w-4 h-4' />
       <Skeleton className='h-4 w-[30%]' />
 
