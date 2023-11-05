8a9509831716e7f10592185b284840d54111b6ba
Jack Najarro <jacknajarro1@gmail.com>
2023-11-04 21:57:03 -0500

    add specify url undefined

--- hooks/use-cover-image.jsx
+++ hooks/use-cover-image.jsx
@@ -3,7 +3,7 @@ import { create } from 'zustand'
 export const useCoverImage = create((set) => ({
   url: undefined,
   isOpen: false,
-  onOpen: () => set({ isOpen: true }),
-  onClose: () => set({ isOpen: false }),
+  onOpen: () => set({ isOpen: true, url: undefined }),
+  onClose: () => set({ isOpen: false, url: undefined }),
   onReplace: (url) => set({ isOpen: true, url })
 }))
